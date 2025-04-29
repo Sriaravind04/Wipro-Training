bd23eb38fff3d500da65016c0b920cc968506013
sri aravind <sriaravindkarthikeyan@gmail.com>
2025-04-29 15:31:19 +0530

    sample1

---
+++ Apr29th/sample1.cs
@@ -0,0 +1,19 @@
+using System;
+
+public class HelloWorld
+{
+     public static void Main(string[] args)
+     {
+         int age = 25;
+         string name = "John";
+         bool isStudent = true;
+         double fees = 55000.75;
+        
+         Console.WriteLine ("Hello world");
+         Console.WriteLine ("Name: " +name);
+         Console.WriteLine ("Age: "+age);
+         Console.WriteLine ("Is Student: " +isStudent);
+     
+       
+     }
+}
