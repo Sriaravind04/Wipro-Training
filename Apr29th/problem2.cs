5477dcc93babe076580714d44af1155be26f6c65
sri aravind <sriaravindkarthikeyan@gmail.com>
2025-04-29 17:02:16 +0530

    problem2

---
+++ Apr29th/problem2.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class HelloWorld
+{
+   
+        
+        static void Main() {
+            for(int i=0;i<=10;i++) {
+               Console.WriteLine("5 x "+i+" = "+(5*i));
+           }
+        }
+         
+       
+     
+}
