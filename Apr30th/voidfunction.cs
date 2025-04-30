11cc7e771a47c6a52da9e407da3571115d880f7a
sri aravind <sriaravindkarthikeyan@gmail.com>
2025-04-30 12:51:45 +0530

    voidfunction

---
+++ Apr30th/voidfunction.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class HelloWorld
+{
+   
+      static void GreetUser(string name) {
+          Console.WriteLine("Hello, "+name);
+      }
+      static void Main()
+      {
+          GreetUser("amina");
+      }
+      
+    }
