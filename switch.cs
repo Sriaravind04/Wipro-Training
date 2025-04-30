f8c10b58e0ce0ecc464e6ba0445e8e0fcce4104d
sri aravind <sriaravindkarthikeyan@gmail.com>
2025-04-30 09:42:29 +0530

    looping

---
+++ switch.cs
@@ -0,0 +1,26 @@
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        int day = 3;
+        
+       switch (day) {
+           case 1:
+           Console.WriteLine("Monday");
+           break;
+           case 2:
+           Console.WriteLine("Tuesday");
+           break;
+           case 3:
+           Console.WriteLine("Wednesday");
+           break;
+           default:
+           Console.WriteLine("Invalid Day");
+           break;
+       }
+       
+        
+    }
+}
