5ec4a2de93cb022e3bc83d43bbe2204f72e2ab0a
sri aravind <sriaravindkarthikeyan@gmail.com>
2025-04-30 10:23:03 +0530

    assignment4

---
+++ Apr30th/excersize4.cs
@@ -0,0 +1,19 @@
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        int a = 1;
+        
+        
+        while(a * a<100) {
+             
+              Console.WriteLine(a*a);
+              a++;
+             
+        }
+        
+      
+    }
+}
