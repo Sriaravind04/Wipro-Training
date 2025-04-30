438527f51556b4114ac241d96e0f25e03690c24b
sri aravind <sriaravindkarthikeyan@gmail.com>
2025-04-30 09:48:53 +0530

    dowhile

---
+++ Apr30th/dowhile.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+      int i =1;
+      
+      do {
+          Console.WriteLine(i);
+          i++;
+      }
+      while(i <= 5);
+       }
+       
+    }
