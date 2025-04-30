b41b2d17a6e4bda575d5e2757819b6ac803e0f94
sri aravind <sriaravindkarthikeyan@gmail.com>
2025-04-30 15:24:06 +0530

    arrays forward and reverse

--- Apr30th/arrays.cs
+++ Apr30th/arrays.cs
@@ -12,3 +12,18 @@ public class HelloWorld
         PrintArray(numbers);
     }
 }
+//reverse
+using System;
+
+public class HelloWorld
+{
+    static void PrintArray(int[] arr) {
+        for(int i=arr.Length-1;i>=0;i--) {
+            Console.WriteLine($"Element {i+1}: {arr[i]}");
+        }
+    }
+    static void Main() {
+        int[] numbers = {10,20,30,40,50};
+        PrintArray(numbers);
+    }
+}
