92aeccb3f1c9d282060e9ae825e7b5e26683283a
Sri aravind k <sriaravindkarthikeyan@gmail.com>
2025-05-05 15:20:37 +0530

    changes

--- May5th/studenttable.sql
+++ May5th/studenttable.sql
@@ -40,3 +40,21 @@ SELECT AVG(Marks) AS AverageMarks FROM Students;
 SELECT Department, MAX(Salary) AS HighestSalary
 FROM Employees
 GROUP BY Department;
+
+
+SELECT FirstName,Marks,
+	CASE
+		WHEN Marks >= 90 THEN 'A'
+		WHEN Marks >= 75 THEN 'B'
+		WHEN Marks >= 60 THEN 'C'
+		ELSE 'D'
+	END AS Grade
+FROM Students;
+
+SELECT Name,Salary,
+	CASE
+		WHEN Salary >=70000 THEN 'High'
+		WHEN Salary >= 50000 THEN 'Medium'
+		ELSE 'Low'
+	END AS SalaryBand
+FROM Employees;
