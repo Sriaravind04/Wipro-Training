c436b32ae576b3b7d1338381f61a12b932fa370d
Sri aravind k <sriaravindkarthikeyan@gmail.com>
2025-05-07 09:25:26 +0530

    task1

--- May7th/task1.cs
+++ May7th/task1.cs
@@ -1,18 +1,30 @@
 using System;
 class Program
 {
-    static void Main() {
+    static void Main()
+    {
         int[] nums = { 1, 2, 2, 3, 4, 4, 4 };
         appears(nums);
 
     }
-    static void appears(int[] nums) {
-        int count = 0;
-        for (int i = 0; i < nums.Length; i++) {
-            for (int j = 0; j < nums.Length; j++) { 
-                if(nums[i] == nums[j]) count++;
-                Console.WriteLine(nums[i] +" appears " + count);
+    static void appears(int[] nums)
+    {
+        bool[] visited = new bool[nums.Length];
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (visited[i]) continue;
+            int count = 1;
+            for (int j = i + 1; j < nums.Length; j++)
+            {
+                if (nums[i] == nums[j])
+                {
+                    count++;
+                    visited[j] = true;
+                }
             }
+
+            Console.WriteLine(nums[i] + " appears " + count);
         }
     }
 }
