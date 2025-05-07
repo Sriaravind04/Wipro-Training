3ec2b6dadee79a3fec35831ef9327ee78c8475e5
Sri aravind k <sriaravindkarthikeyan@gmail.com>
2025-05-07 10:09:33 +0530

    task3

---
+++ May7th/task3.cs
@@ -0,0 +1,36 @@
+using System;
+class Program
+{
+    static void PrintDuplicate(string sentence)
+    {
+        string[] words = sentence.ToLower().Split(' ');
+        Dictionary<string, int> wordCount = new Dictionary<string, int>();
+        List<string> duplicates = new List<string>();
+
+        foreach (string word in words) {
+            if (wordCount.ContainsKey(word))
+            {
+                wordCount[word]++;  
+
+
+            }
+            else
+            {
+                wordCount[word] = 1; 
+                }
+            }
+        foreach (var item in wordCount)
+        {
+            if (item.Value > 1)
+            { 
+                duplicates.Add(item.Key);   
+            }
+        }
+        Console.WriteLine("duplicate words: "+ string.Join(", ", duplicates));  
+        }
+    static void Main()
+    {
+        string input = "this is a test and this test is fun";
+        PrintDuplicate(input);
+    }
+}
