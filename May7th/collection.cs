b3f57f3d1386003a649274cd5c446ba8590e38a5
Sri aravind k <sriaravindkarthikeyan@gmail.com>
2025-05-07 12:05:31 +0530

    collection

---
+++ May7th/collection.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+class Program
+{
+    static void Main()
+    {
+        ArrayList list = new ArrayList();
+        list.Add("apple");
+        list.Add(42);
+        list.Add(3.14);
+
+        foreach (var item in list)
+        {
+            Console.WriteLine(item);
+        }
+        Console.Read();
+    }
+}
