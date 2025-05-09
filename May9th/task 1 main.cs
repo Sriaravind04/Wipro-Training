f1e1a57a48f90b568c74264e6f917821449891c3
Sri aravind k <sriaravindkarthikeyan@gmail.com>
2025-05-09 16:15:24 +0530

    main task1

---
+++ May9th/task 1 main.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class program2
+    {
+        static void Main()
+        {
+           Circle circle = new Circle();
+            circle.Draw();
+        }
+    }
+}
