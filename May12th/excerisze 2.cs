755069632901be0cc2bde5e5802223564c1ac137
Sri aravind k <sriaravindkarthikeyan@gmail.com>
2025-05-12 14:52:22 +0530

    excersize 2

---
+++ May12th/excerisze 2.cs
@@ -0,0 +1,43 @@
+using System;
+
+interface IAnimalActions
+{
+    void Eat();
+    void Sleep();
+}
+
+class Lion : IAnimalActions
+{
+    public void Eat() {
+        Console.WriteLine("Lion is eating meat");
+    }
+    public void Sleep()
+    {
+        Console.WriteLine("Lion is sleeping");
+    }
+}
+
+class Elephant : IAnimalActions
+{
+    public void Eat()
+    {
+        Console.WriteLine("Elephant is eating grass");
+    }
+    public void Sleep() {
+        Console.WriteLine("Elephant is sleeping");
+    }
+}
+
+class Program
+{
+    static void Main() { 
+        IAnimalActions king = new Lion();
+        IAnimalActions general = new Elephant();
+
+        king.Eat();
+        king.Sleep();
+
+        general.Sleep();
+        general.Eat();
+    }
+}
