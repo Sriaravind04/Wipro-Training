daf28db562239ad1cd358bff26316909a5dbb37a
Sri aravind k <sriaravindkarthikeyan@gmail.com>
2025-05-13 10:59:52 +0530

    delegate

--- May13th/Delegate.cs
+++ May13th/Delegate.cs
@@ -18,14 +18,31 @@ namespace ConsoleApp4
             { 
                 Console.WriteLine($" Hello, {s}!");
             }
+            static void Goodbye(string s) {
+                Console.WriteLine($" Goodbye, {s}!");
+            }
             static void Main()
             {
                 //Declare instance of the custom delegate
-                CustomDel hiDel;
-
+                CustomDel hiDel,byeDel,multiDel;
+               
                 hiDel = Hello;
-                Console.WriteLine("Invoking delegate hiDel:");
+                //Create the delegate object byedel that reference the
+                //method goodbye.
+                byeDel = Goodbye;
+
+                //the two delegates , hidel and byedel , are combined 
+                //toform multidel.
+                //MULTICAST DELEGATE - SIMPLE INVOCATION
+                multiDel = hiDel + byeDel;
+
+                Console.WriteLine("Invoking delegate hiDel: ");
                 hiDel("A");
+                Console.WriteLine("Invoking delegate byeDel:");
+                byeDel("B");
+                Console.WriteLine("Invoking delegate multiDel:");
+                    multiDel("C");
+               
             }
         }
     }
