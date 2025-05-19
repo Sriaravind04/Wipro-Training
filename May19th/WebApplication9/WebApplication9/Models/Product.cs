59a2df800d8cbe136291741c621b0b2f3f29ed17
Sri aravind k <sriaravindkarthikeyan@gmail.com>
2025-05-19 17:35:18 +0530

    product migration add,update

---
+++ May19th/WebApplication9/WebApplication9/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+namespace WebApplication9.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }
+
+        public string ProductDescription { get; set; }
+
+      
+    }
+    
+}
