f8f64f52fd2d24cef24bf4d213b933b3ff582464
Sri aravind k <sriaravindkarthikeyan@gmail.com>
2025-05-19 17:32:14 +0530

    student migration

---
+++ May19th/WebApplication7/WebApplication7/Models/Student.cs
@@ -0,0 +1,19 @@
+﻿namespace WebApplication7.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Location { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public Grade Grade { get; set; }
+    }
+    public class Grade { 
+        public int GradeId { get; set; }
+        public string GradeName { get; set; }
+        public string Section { get; set; }
+        public ICollection<Student> Students { get; set; }
+    }
+}
