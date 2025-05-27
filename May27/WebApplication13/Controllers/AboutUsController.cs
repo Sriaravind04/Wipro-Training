70fa2ed94ef3be609f082d125f0f2b7dc6622483
Sri aravind k <sriaravindkarthikeyan@gmail.com>
2025-05-27 14:31:21 +0530

    webpage

---
+++ May27/WebApplication13/Controllers/AboutUsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication13.Controllers
+{
+    public class AboutUsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
