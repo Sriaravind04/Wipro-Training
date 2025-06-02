dfb68f24bd4f7c59172977fa1896d25eae4497d3
Sri aravind k <sriaravindkarthikeyan@gmail.com>
2025-06-02 17:45:06 +0530

    web api task employee

---
+++ June 2/WebApplication18/WebApplication18/Data/WebApplication18Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApplication18.Models;
+
+namespace WebApplication18.Data
+{
+    public class WebApplication18Context : DbContext
+    {
+        public WebApplication18Context (DbContextOptions<WebApplication18Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebApplication18.Models.Employee> Employee { get; set; } = default!;
+    }
+}
